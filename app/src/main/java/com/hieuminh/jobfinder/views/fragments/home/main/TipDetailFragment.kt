d0dbc9c939e8be4d011fd774f68ce9d68a6621c8
hieuminh89 <hieuminh22490@gmail.com>
2022-09-18 10:39:44 +0700

    Show tip detail

--- app/src/main/java/com/hieuminh/jobfinder/views/fragments/home/main/TipDetailFragment.kt
+++ app/src/main/java/com/hieuminh/jobfinder/views/fragments/home/main/TipDetailFragment.kt
@@ -1,18 +1,32 @@
 package com.hieuminh.jobfinder.views.fragments.home.main
 
-import com.hieuminh.jobfinder.common.extensions.ViewExtensions.navController
-import com.hieuminh.jobfinder.common.extensions.ViewExtensions.onClick
+import android.os.Bundle
 import com.hieuminh.jobfinder.databinding.FragmentTipDetailBinding
+import com.hieuminh.jobfinder.models.Tip
 import com.hieuminh.jobfinder.views.fragments.base.BaseFragment
 
 class TipDetailFragment : BaseFragment<FragmentTipDetailBinding>() {
+    private lateinit var tip: Tip
 
     override fun getViewBinding() = FragmentTipDetailBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            val args = TipDetailFragmentArgs.fromBundle(it)
+            tip = args.tip
+        }
+    }
 
     override fun initListener() {
     }
 
     override fun initView() {
+        binding.tvTitle.text = tip.title
+        binding.tvViewTitle.text = tip.title
+        binding.tvAuthorName.text = tip.authorName
+        binding.tvAuthorPosition.text = tip.authorPosition
+        binding.tvContent.text = tip.content
     }
 
     companion object {
